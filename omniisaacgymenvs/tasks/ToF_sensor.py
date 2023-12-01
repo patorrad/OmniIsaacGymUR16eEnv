a9c4f697e914ff5fcf2795aa6705cb5364064a2b
Entong Su <ensu@ucsd.edu>
2023-12-01 14:33:52 -0800

    temp work

--- omniisaacgymenvs/tasks/ToF_sensor.py
+++ omniisaacgymenvs/tasks/ToF_sensor.py
@@ -112,31 +112,31 @@ import numpy as np
 from omni.isaac.core import World
 from omni.isaac.core.objects import cuboid, sphere
 
-# CuRobo
-from curobo.cuda_robot_model.cuda_robot_model import CudaRobotModel
+# # CuRobo
+# from curobo.cuda_robot_model.cuda_robot_model import CudaRobotModel
 
+# # from curobo.wrap.reacher.ik_solver import IKSolver, IKSolverConfig
+# from curobo.geom.sdf.world import CollisionCheckerType
+# from curobo.geom.types import WorldConfig
+# from curobo.rollout.rollout_base import Goal
+# from curobo.types.base import TensorDeviceType
+# from curobo.types.math import Pose
+# from curobo.types.robot import JointState, RobotConfig
+# from curobo.types.state import JointState
+# from curobo.util.logger import setup_curobo_logger
+# from curobo.util.usd_helper import UsdHelper
+# from curobo.util_file import (
+#     get_assets_path,
+#     get_filename,
+#     get_path_of_dir,
+#     get_robot_configs_path,
+#     get_world_configs_path,
+#     join_path,
+#     load_yaml,
+# )
 # from curobo.wrap.reacher.ik_solver import IKSolver, IKSolverConfig
-from curobo.geom.sdf.world import CollisionCheckerType
-from curobo.geom.types import WorldConfig
-from curobo.rollout.rollout_base import Goal
-from curobo.types.base import TensorDeviceType
-from curobo.types.math import Pose
-from curobo.types.robot import JointState, RobotConfig
-from curobo.types.state import JointState
-from curobo.util.logger import setup_curobo_logger
-from curobo.util.usd_helper import UsdHelper
-from curobo.util_file import (
-    get_assets_path,
-    get_filename,
-    get_path_of_dir,
-    get_robot_configs_path,
-    get_world_configs_path,
-    join_path,
-    load_yaml,
-)
-from curobo.wrap.reacher.ik_solver import IKSolver, IKSolverConfig
-from curobo.wrap.reacher.motion_gen import MotionGen, MotionGenConfig, MotionGenPlanConfig
-from curobo.wrap.reacher.mpc import MpcSolver, MpcSolverConfig
+# from curobo.wrap.reacher.motion_gen import MotionGen, MotionGenConfig, MotionGenPlanConfig
+# from curobo.wrap.reacher.mpc import MpcSolver, MpcSolverConfig
 
 
 class TofSensorTask(RLTask):
