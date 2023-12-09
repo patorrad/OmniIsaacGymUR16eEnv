c06a333d40f71a2dd06cabc7a3fab68529760028
Entong Su <ensu@ucsd.edu>
2023-12-08 22:24:55 -0800

    fix action bugs

--- omniisaacgymenvs/tasks/ToF_sensor.py
+++ omniisaacgymenvs/tasks/ToF_sensor.py
@@ -823,7 +823,7 @@ class TofSensorTask(RLTask):
         # delta pose
         action = torch.clip(action, -1, 1)
         # self.pre_action[:, 5] = action.reshape(-1) * 0
-        self.pre_action[:, [0, 1, 2, 3, 4, 5]] = action * 0
+        self.pre_action[:, [0, 1, 2, 3, 4, 5]] = action
 
         # action[:,[0,1,2,3,4]] = 0 # rotate along z axis to rotation
 
